74067e22158a48060264ba62ca81dbb0c8d1b039
kdjlyy <2369834554@qq.com>
2020-06-17 23:50:29 +0800

    0v0

--- HelloWorld.py
+++ HelloWorld.py
@@ -2,3 +2,8 @@ message = "hello python world!"
 print(message.title())
 print(message.upper())
 print(message.lower())
+
+first_name = "ada"
+last_name = "lovelace"
+full_name = first_name + " " + last_name
+print(full_name)
