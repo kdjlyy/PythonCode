be07086716d704f42f23282147ecd382606fe9ae
kdjlyy <2369834554@qq.com>
2020-07-12 21:36:50 +0800

    ~_~

--- Chapter 2.py
+++ Chapter 2.py
@@ -1,16 +1,16 @@
 # 第二章 变量和简单数据类型
 message = "hello python world!"
-print(message.title())  #Hello Python World!
-print(message.upper())  #HELLO PYTHON WORLD!
-print(message.lower())  #hello python world!
+print(message.title())  # Hello Python World!
+print(message.upper())  # HELLO PYTHON WORLD!
+print(message.lower())  # hello python world!
 ##################################################
 first_name = "ada"
 last_name = "lovelace"
 full_name = first_name + " " + last_name
-print(full_name)  #ada lovelace
-print("Hello, " + full_name.title() + "!")  #Hello, Ada Lovelace!
+print(full_name)  # ada lovelace
+print("Hello, " + full_name.title() + "!")  # Hello, Ada Lovelace!
 message = "Hello, " + full_name.title() + "!"
-print(message)  #Hello, Ada Lovelace!
+print(message)  # Hello, Ada Lovelace!
 ##################################################
 print("\n\tPython")
 ##################################################
@@ -18,10 +18,10 @@ print("\n\tPython")
 # lstrip() 删除字符串开头空白部分
 # rstrip() 删除字符串末尾空白部分
 favorite_language = "  Python Language "
-print(favorite_language.strip())  #Python Language
-print(favorite_language.lstrip())  #Python Language
-print(favorite_language.rstrip())  #  Python Language
-print(favorite_language.replace(" ", ""))  #PythonLanguage
+print(favorite_language.strip())  # Python Language
+print(favorite_language.lstrip())  # Python Language
+print(favorite_language.rstrip())  # __Python Language
+print(favorite_language.replace(" ", ""))  # PythonLanguage
 
 age = 21
 message = "Happy " + str(age) + "rd Birthday!"
--- Chapter 3.py
+++ Chapter 3.py
@@ -1,1 +1,56 @@
 # 第三章 列表简介
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles)  # ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles[0])  # trek
+print(bicycles[0].title())  # Trek
+print(bicycles[-1])  # specialized
+########################################################
+# 修改列表元素
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles)  # ['honda', 'yamaha', 'suzuki']
+motorcycles[0] = 'ducati'
+print(motorcycles)  # ['ducati', 'yamaha', 'suzuki']
+# 在列表中添加元素 append()、insert()
+motorcycles = ['honda', 'yamaha', 'suzuki']
+motorcycles.append('ducati')
+print(motorcycles)  # ['honda', 'yamaha', 'suzuki', 'ducati']
+
+motorcycles = ['honda', 'yamaha', 'suzuki']
+motorcycles.insert(0, 'ducati')  # insert()将‘ducati’存储到0这个位置
+print(motorcycles)  # ['ducati', 'honda', 'yamaha', 'suzuki']
+#从列表中删除元素 del语句、pop()、remove()
+motorcycles = ['honda', 'yamaha', 'suzuki']
+del motorcycles[0]
+print(motorcycles)  # ['yamaha', 'suzuki']
+
+motorcycles = ['honda', 'yamaha', 'suzuki']
+poped_motorcycle = motorcycles.pop()  # pop()可以弹出列表末尾元素
+print(poped_motorcycle)  # suzuki
+print(motorcycles)  # ['honda', 'yamaha']
+motorcycles = ['honda', 'yamaha', 'suzuki']
+first_owned = motorcycles.pop(1)  # pop(index)弹出列表任意位置元素
+print(first_owned)  # yamaha
+print(motorcycles)  # ['honda', 'suzuki']
+
+motorcycles = ['honda', 'yamaha', 'suzuki', 'ducati', 'ducati']
+motorcycles.remove('ducati')  # 根据键值删除元素，remove()只删除第一个指定的值
+print(motorcycles)  # ['honda', 'yamaha', 'suzuki', 'ducati']
+# 组织列表的方式
+# list.sort()对列表进行永久性排序、list.sorted()对列表进行临时排序
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+cars.sort()  # 错误写法：print(cars.sort())，因为sort()无返回值
+print(cars)  # ['audi', 'bmw', 'subaru', 'toyota']
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+cars.sort(reverse=True)  # 按字母顺序逆向排序
+print(cars)  # ['toyota', 'subaru', 'bmw', 'audi']
+
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+print(sorted(cars))  # ['audi', 'bmw', 'subaru', 'toyota']
+print(cars)  # ['bmw', 'audi', 'toyota', 'subaru']
+print(sorted(cars, reverse=True))  # ['toyota', 'subaru', 'bmw', 'audi']
+
+
+
+
+
+
